91544b7e758b4be8186f65b289debdf14c855ba1
LazyPrxncess <nordgren.cameron@gmail.com>
2025-02-08 18:03:27 -0700

    created word count framework

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 def main():
-    with open("books/Frankenstein.txt") as f:
+    with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+    print(file_contents)
+
+def count_words():
+    word_count = 0
+    words = []
+    with open("books/frankenstein.txt") as f:
+        for words in f:
+            words = f.split()
+            for word in words:
+                word_count += 1
+    print (word_count)
 main()
