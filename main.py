5a2184776a6075c8192a597e22ada044c8e4a567
LazyPrxncess <nordgren.cameron@gmail.com>
2025-02-08 19:02:37 -0700

    Finalized count_words and added a master call function

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-def main():
+def file_contents():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
 
 def count_words():
     word_count = 0
-    words = []
     with open("books/frankenstein.txt") as f:
-        for words in f:
-            words = f.split()
+        for contents in f:
+            words = contents.split()
             for word in words:
                 word_count += 1
     print (word_count)
-main()
+
+def main():
+    file_contents()
+    count_words()
+if __name__ == "__main__":
+    main()
