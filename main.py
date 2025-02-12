0380df40f34921cfc4db6f6e8477b22195c36dfd
LazyPrxncess <nordgren.cameron@gmail.com>
2025-02-12 12:17:22 -0700

    Modified init to be more universal, completed Bookbot

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
-def file_contents():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+class BookAnalyzer:
+    def __init__(self, filename, book_name):
+        self.filename = filename
+        self.book_name = book_name
+        with open(self.filename) as f:
+            self.text = f.read()
+        self.character_dict = {}
+        print(f"--- Begin report of {self.filename} ---")
 
-def count_words():
-    word_count = 0
-    with open("books/frankenstein.txt") as f:
-        for contents in f:
-            words = contents.split()
-            for word in words:
-                word_count += 1
-    print (word_count)
+    def file_contents(self):
+        print(f"\n=== Contents of {self.book_name} ===")
+        
+        print(self.text)
+        return self.text
+
+    def count_words(self):
+        word_count = 0
+        text = self.text
+        words = text.split()
+        word_count = len(words)
+        print (f"\n{word_count} words were found in the document")
+        return word_count
+    
+    def count_characters(self):
+        book_text = self.text
+        lowered_string = book_text.lower()
+        for character in lowered_string:
+            if character not in self.character_dict:
+                self.character_dict[character] = 1
+            else:
+                self.character_dict[character] += 1
+        return self.character_dict
+    
+    def alphabet_only(self):
+        alpha_dict = {}
+        for char in self.character_dict:
+            if char.isalpha():
+                alpha_dict[char] = self.character_dict[char]
+        for key in sorted(alpha_dict, key=lambda x: alpha_dict[x], reverse=True):
+            if alpha_dict[key] == 1:
+                print(f"The '{key}' character was found {alpha_dict[key]} time")
+            else:
+                print(f"The '{key}' character was found {alpha_dict[key]} times")
+        
 
 def main():
-    file_contents()
-    count_words()
+    frankenstein = BookAnalyzer("books/frankenstein.txt", "Frankenstein")
+    frankenstein.count_words()
+    frankenstein.count_characters()
+    frankenstein.alphabet_only()
+    print(f"--- End report ---")
+
+
 if __name__ == "__main__":
     main()
