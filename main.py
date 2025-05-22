8961a2cbee7db7bc720d39b4972f5683df861538
LazyPrxncess <nordgren.cameron@gmail.com>
2025-05-22 12:24:00 -0600

    Modified  and  functions to have a file path input. Changed character count format.

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+import sys
+
 class BookAnalyzer:
-    def __init__(self, filename, book_name):
-        self.filename = filename
-        self.book_name = book_name
+    def __init__(self, filepath):
+        self.filename = filepath
+        self.book_name = filepath.split("/")[-1].split(".")[0]
         with open(self.filename) as f:
             self.text = f.read()
         self.character_dict = {}
         print(f"--- Begin report of {self.filename} ---")
 
     def file_contents(self):
-        print(f"\n=== Contents of {self.book_name} ===")
-        
-        print(self.text)
         return self.text
 
     def count_words(self):
@@ -22,9 +21,7 @@ class BookAnalyzer:
         return word_count
     
     def count_characters(self):
-        book_text = self.text
-        lowered_string = book_text.lower()
-        for character in lowered_string:
+        for character in self.text.lower():
             if character not in self.character_dict:
                 self.character_dict[character] = 1
             else:
@@ -38,18 +35,21 @@ class BookAnalyzer:
                 alpha_dict[char] = self.character_dict[char]
         for key in sorted(alpha_dict, key=lambda x: alpha_dict[x], reverse=True):
             if alpha_dict[key] == 1:
-                print(f"The '{key}' character was found {alpha_dict[key]} time")
+                print(f"{key}: {alpha_dict[key]}")
             else:
-                print(f"The '{key}' character was found {alpha_dict[key]} times")
-        
+                print(f"{key}: {alpha_dict[key]}")
 
 def main():
-    frankenstein = BookAnalyzer("books/frankenstein.txt", "Frankenstein")
-    frankenstein.count_words()
-    frankenstein.count_characters()
-    frankenstein.alphabet_only()
-    print(f"--- End report ---")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        print("\nTry 'books/[book_name].txt' for example")
+        sys.exit(1)
 
+    book_analyzer = BookAnalyzer(sys.argv[1])
+    print(f"{book_analyzer.count_words(book_analyzer.text)} words found in the document")
+    book_analyzer.count_characters()
+    book_analyzer.alphabet_only()
+    print(f"--- End report ---")
 
 if __name__ == "__main__":
     main()
